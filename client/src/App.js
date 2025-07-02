528c0c55b24a29243b203131013fab6128c4470d
Sujoy Paul <149752139+SujoyPaul0@users.noreply.github.com>
2025-07-02 15:40:05 +0530

    Initial setup and CSS

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { ThemeProvider, styled } from "styled-components";
+import {lightTheme} from "./utils/Themes";
+import {BrowserRouter} from "react-router-dom";
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+flex-direction: column;
+background: ${({ theme }) => theme.bg};
+color: ${({ theme }) => theme.text_primary}
+overflow-x: hidden;
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+      <Container>
+      </Container></BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
